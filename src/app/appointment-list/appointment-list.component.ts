e6eb15d29f190ceb8e3a402d265c4a9247727a76
Alvyn <banerjee.alvin@gmail.com>
2023-12-16 18:20:03 +0530

    Using one way data binding in appointment-list.component.html

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrl: './appointment-list.component.css'
 })
 export class AppointmentListComponent {
+  appointment:string="This value is goint to be used in the HTML"
 
 }
