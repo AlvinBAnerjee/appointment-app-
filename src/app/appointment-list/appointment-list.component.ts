65a3b790c6da741f02ebee71c8f0e3270b84be79
Alvyn <banerjee.alvin@gmail.com>
2023-12-16 19:44:51 +0530

    Adding local storage functionality

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
 import { title } from 'process';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrl: './appointment-list.component.css'
 })
-export class AppointmentListComponent {
+export class AppointmentListComponent  implements OnInit{
+  
   newAppointmentTitle:string="";
   newAppointmentDate: Date=new Date();
   appointments: Appointment[]=[];
   id:number=this.appointments.length;
+
+  ngOnInit(): void {
+    let savedAppointment=localStorage.getItem("appointments");
+    this.appointments=savedAppointment?JSON.parse(savedAppointment):[]
+    this.id=savedAppointment?this.appointments[this.appointments.length-1].id+1 : 0
+  }
 
 
   addAppointment():void{
@@ -24,6 +32,8 @@ export class AppointmentListComponent {
       }
 
       this.appointments.push(newAppointment)
+      localStorage.setItem("appointments", JSON.stringify(this.appointments))
+
 
      
     }
@@ -37,6 +47,7 @@ export class AppointmentListComponent {
 
   deleteAppointment(index:number):void{
     this.appointments.splice(index,1);
+    localStorage.setItem("appointments", JSON.stringify(this.appointments))
 
   }
 }
